b918fdb04eb579e0c701952a02eda23e6851b486
Yunfei-Ma-McMaster <Yunfei-Ma-McMaster@users.noreply.github.com>
2023-01-08 19:05:05 +0000

    jekyll build from Action 74394b72548e224ba0cff31432bdb5cf7d76679c

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Portfolio Diversification: The Dirty Truth",
+        "excerpt":"In this article, a novel perspective toward portfolio diversification is presented for retail investor to understand how they are manipulated by Wall Street to play the game they will never win… The wildest stock in the last few years is undoubtedly Tesla Inc. With a visionary leader Elon Musk, a...","categories": [],
+        "tags": [],
+        "url": "/prof-diveristy",
+        "teaser": null
+      }]
