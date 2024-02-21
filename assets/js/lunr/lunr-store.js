2ae73d69e6a27399b6995694f7e09b1e5459f126
Yunfei-Ma-McMaster <Yunfei-Ma-McMaster@users.noreply.github.com>
2024-02-21 22:12:11 +0000

    jekyll build from Action b0fe947a419499412eb70a6479ae9ba4335a9304

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "The Dirty Truth about Portfolio Diversification",
+        "excerpt":"DON’T PUT ALL YOUR EGGS IN ONE BASKET Many investors swear by diversification. Spread your investments across a range of assets, and you’re less likely to lose money, right? That’s the theory I followed when I first started investing. After years of interacting with fellow retail investors who have outlier...","categories": [],
+        "tags": [],
+        "url": "/posts/diversification",
+        "teaser": null
+      },{
+        "title": "What is the meaning of my name?",
+        "excerpt":"Yunfei (first name) Ma (last name), or Ma Yunfei, is written in Chinese characters as 马云飞. 马 (Mǎ) means “horse”, a symbol of speed and freedom. It is 13th most common surname in mainland China. 云 (Yún) means “cloud”, representing elevation, flexibility, and a sense of dreaminess. It symbolizes happiness...","categories": [],
+        "tags": [],
+        "url": "/posts/yunfei-ma-meaning",
+        "teaser": null
+      }]
