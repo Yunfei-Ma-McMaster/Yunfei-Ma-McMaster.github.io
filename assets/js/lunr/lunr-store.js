43cae1f6a4a99d1934270492fe8c88db90a4d941
Yunfei-Ma-McMaster <Yunfei-Ma-McMaster@users.noreply.github.com>
2024-10-15 01:08:06 +0000

    jekyll build from Action 4da21155945e283e9d359a87780631be259f3265

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "The Dirty Truth about Portfolio Diversification",
+        "excerpt":"DON’T PUT ALL YOUR EGGS IN ONE BASKET Many investors swear by diversification. Spread your investments across a range of assets, and you’re less likely to lose money, right? That’s the theory I followed when I first started investing. After years of interacting with fellow retail investors who have outlier...","categories": [],
+        "tags": [],
+        "url": "/posts/diversification",
+        "teaser": null
+      },{
+        "title": "What is the meaning of my name?",
+        "excerpt":"Yunfei (first name) Ma (last name), or Ma Yunfei, is written in simplified Chinese as 马云飞 and traditional Chinese as 馬雲飛. 马 (Mǎ) means “horse”, a symbol of speed and freedom. It is 13th most common surname in mainland China. 云 (Yún) means “cloud”, representing elevation, flexibility, and a sense...","categories": [],
+        "tags": [],
+        "url": "/posts/yunfei-ma-meaning",
+        "teaser": null
+      },{
+        "title": "Biography of the Ma Family",
+        "excerpt":"This text is a biography of the Ma family, compiled in 1983, and it provides a detailed account of my family’s history and struggles in Sichuan, China. Starting with the late Ming and early Qing dynasties, the text recounts the family’s hardship, poverty, and resilience in the face of various...","categories": [],
+        "tags": [],
+        "url": "/posts/ma-biography",
+        "teaser": null
+      }]
